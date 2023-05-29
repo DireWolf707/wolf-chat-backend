4fa23b5f53f538f898446164fe1cd6017aea32a7
DireWolf707 <rahulsood707@gmail.com>
2023-05-29 17:55:48 +0530

    added schema

--- src/drizzle/schema.js
+++ src/drizzle/schema.js
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uniqueIndex, index, uuid, varchar, primaryKey, foreignKey } from "drizzle-orm/pg-core"
+import { pgTable, text, timestamp, uniqueIndex, index, uuid, varchar, primaryKey, foreignKey, boolean } from "drizzle-orm/pg-core"
 import { createInsertSchema } from "drizzle-zod"
 
 export const userT = pgTable(
@@ -18,3 +18,56 @@ export const userT = pgTable(
 )
 
 export const userSchema = createInsertSchema(userT)
+
+export const friendT = pgTable(
+  "Friend",
+  {
+    to: uuid("to")
+      .references(() => userT.id)
+      .notNull(),
+    from: uuid("from")
+      .references(() => userT.id)
+      .notNull(),
+    isRequestAccepted: boolean("is_request_accepted").default(false).notNull(),
+  },
+  (friendT) => ({
+    pk: primaryKey(friendT.to, friendT.from),
+    oppositeIdx: uniqueIndex(friendT.from, friendT.to), // until unique constraint is availabe in drizzle
+  })
+)
+
+export const chatT = pgTable("Chat", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  userId: uuid("user_id")
+    .references(() => userT.id)
+    .notNull(),
+  chatRoomId: uuid("chat_room_id")
+    .references(() => chatRoomT.id)
+    .notNull(),
+  type: varchar("type", { length: 6, enum: ["text", "img", "vid"] }).notNull(),
+  content: varchar("content", { length: 255 }).notNull(),
+})
+
+export const chatRoomT = pgTable("ChatRoom", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+  lastChatId: uuid("last_chat_id").references(() => chatT.id),
+  isGroup: boolean("is_group").default(false).notNull(),
+  name: varchar("name", { length: 255 }),
+})
+
+export const participantT = pgTable(
+  "Participant",
+  {
+    userId: uuid("user_id")
+      .references(() => userT.id)
+      .notNull(),
+    chatRoomId: uuid("chat_room_id")
+      .references(() => chatRoomT.id)
+      .notNull(),
+    lastChatSeenId: uuid("last_chat_id").references(() => chatT.id),
+  },
+  (participantT) => ({
+    pk: primaryKey(participantT.userId, participantT.chatRoomId),
+  })
+)
